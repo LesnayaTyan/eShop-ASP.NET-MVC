35c263657e1f6ed7f081bbec185a98570d813116
LesnayaTyan <lesnayatyan@seznam.cz>
2023-03-26 12:50:20 +0200

    Adding Models and Indexes of Controllers

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,41 @@
+﻿using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {          
+
+        }
+
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Actor_Movie>().HasKey(actormovie => new
+            {
+            actormovie.ActorId,
+            actormovie.MovieId});
+
+            modelBuilder.Entity<Actor_Movie>().HasOne(movie =>
+            movie.Movie).WithMany(actormovie =>
+            actormovie.Actors_Movies).HasForeignKey(movie => movie.MovieId);
+
+
+            modelBuilder.Entity<Actor_Movie>().HasOne(movie =>
+            movie.Actor).WithMany(actormovie =>
+            actormovie.Actors_Movies).HasForeignKey(movie => movie.ActorId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Actor_Movie> Actors_Movies { get; set; }
+        public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<Producer> Producers { get; set; }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
+using eTickets.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Add DbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -24,4 +32,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+//Seed database
+AppDbInitializer.Seed(app);
 app.Run();
