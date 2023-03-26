86f92a3cb216253fc6cdee7645f25a0d2f711a7d
LesnayaTyan <lesnayatyan@seznam.cz>
2023-03-26 15:31:46 +0200

    Adding Actors View

--- Models/Actor.cs
+++ Models/Actor.cs
@@ -7,8 +7,11 @@ namespace eTickets.Models
         [Key]
         public int Id { get; set; }
 
+        [Display(Name ="Profile Picture Url")]
         public string ProfilePicture { get; set; }
+        [Display(Name = "Full Name")]
         public string FullName { get; set; }
+        [Display(Name = "Bio")]
         public string Bio { get; set; }
 
         public List<Actor_Movie> Actors_Movies { get; set;}
